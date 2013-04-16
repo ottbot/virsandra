022c8764637dfdcd430409b069a8a30fee197caa
Robert Crim <rob@servermilk.com>
2013-04-16 23:16:23 +0100

    Create and destroy test keyspace in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,27 @@ SimpleCov.start
 require 'rspec'
 require 'virsandra'
 
+
+TEST_KEYSPACE = "virtest"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  config.before(:each) do
-    Virsandra.configure do |v|
-      v.keyspace = 'virtest'
-    end
+  config.before(:all) do
+    Virsandra.keyspace = 'system'
+    Virsandra.execute("CREATE KEYSPACE #{TEST_KEYSPACE} WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+    Virsandra.reset!
   end
 
+  config.before(:each) do
+    Virsandra.keyspace = TEST_KEYSPACE
+  end
+
+  config.after(:all) do
+    Virsandra.execute("DROP KEYSPACE #{TEST_KEYSPACE}");
+  end
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
