029a95d8819a8070a28aeb6f0ce5ecca4e2128b6
Robert Crim <rob@servermilk.com>
2013-04-17 19:00:58 +0100

    Safer build-up for test keyspace, create a "companies" table

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,26 +8,62 @@ require 'virsandra'
 
 TEST_KEYSPACE = "virtest"
 
+def create_keyspace
+  Virsandra.keyspace = 'system'
+  Virsandra.execute("CREATE KEYSPACE #{TEST_KEYSPACE} WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+  Virsandra.reset!
+end
+
+def create_companies_table
+  cql = <<-CQL
+     CREATE TABLE companies (
+        id uuid,
+        name text,
+        score int,
+        founder text,
+        founded int,
+        PRIMARY KEY (id, score))
+  CQL
+  Virsandra.keyspace = TEST_KEYSPACE
+  Virsandra.execute(cql)
+end
+
+def drop_keyspace
+  Virsandra.reset!
+  Virsandra.keyspace = 'system'
+  Virsandra.execute("DROP KEYSPACE #{TEST_KEYSPACE}")
+end
+
+def build_up
+  begin
+    create_keyspace
+    create_companies_table
+  rescue CassandraCQL::Error::InvalidRequestException
+    drop_keyspace
+
+    if defined?(retried)
+      raise $!
+    else
+      retried = true
+      retry
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  config.before(:all) do
-    Virsandra.keyspace = 'system'
-    Virsandra.execute("CREATE KEYSPACE #{TEST_KEYSPACE} WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
-    Virsandra.reset!
+  config.before(:suite) do
+    build_up
   end
 
-  config.before(:each) do
+  config.before do
     Virsandra.reset!
     Virsandra.keyspace = TEST_KEYSPACE
   end
 
-  config.after(:all) do
-    Virsandra.keyspace = 'system'
-    Virsandra.execute("DROP KEYSPACE #{TEST_KEYSPACE}");
-  end
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
